c83f6af4567f9e8f4cc0ae89b9d8b8521a06b379
Michael Pennington <michaeldanielpennington@gmail.com>
2025-08-27 21:58:43 -0400

    Implement automated table detection/formatting

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -8,52 +8,31 @@
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
     document.getElementById("formatTransactions").onclick = formatTable;
   }
 });
 
-export async function run() {
-  try {
-    await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 export async function formatTable() {
   try {
     await Excel.run(async (context) => {
-      const worksheet = context.workbook.worksheets.getActiveWorksheet();
-      const range = worksheet.getRange("A1:A1000");
-      range.load("text");
+      const sheet = context.workbook.worksheets.getActiveWorksheet();
+      const range = sheet.getUsedRange();
+      let dispTable = sheet.tables.add(range, true);
+
       await context.sync();
-      let contents = range.text[0];
-      var tableEnd = "";
-      for (let i = 1; i < 1000; i++) {
-        if (contents[i] === "") {
-          tableEnd = `AV${i + 1}`;
-          break;
-        }
+
+      for (let column of ["pi", "pn", "mogroup", "mophysn_na", "mo_qty",
+        "give_units", "dose_text", "route", "start_dati", "stop_dati",
+        "give_amt", "dose_max", "mas_desc", "ymi_grp", "bs_id", "bs_name",
+        "bs_suffix"]) {
+        console.log(column);
+        dispTable.columns.getItem(column).delete();
       }
-      let dispTable = worksheet.tables.add(`A1:${tableEnd}`, true);
 
-      // worksheet.activate();
-
+      if (Office.context.requirements.isSetSupported("ExcelApi", "1.2")) {
+        dispTable.getRange().format.autofitRows();
+        dispTable.getRange().format.autofitColumns();
+      }
       await context.sync();
     });
   } catch (error) {
